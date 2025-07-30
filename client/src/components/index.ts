bc59c491c92eb758af6d4f2d017f1a6dfb09e915
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2025-07-30 19:58:55 +0530

    chore(client): add package-lock and required index.ts files

---
+++ client/src/components/index.ts
@@ -0,0 +1,20 @@
+export { ErrorMessage } from "./common/ErrorMessage";
+export { LoaderSpin } from "./common/LoaderSpin";
+export { LoadingOverlay } from "./common/LoadingOverlay";
+export { PasswordInput } from "./common/PasswordInput";
+export { FlowledgeBranding } from "./common/FlowledgeBranding";
+export { EmptyState } from "./common/EmptyState";
+export { ErrorState } from "./common/ErrorState";
+
+export { PageTransition } from "./Layout/PageTransition";
+export { SonnerToaster } from "./Layout/SonnerToaster";
+export { NotFoundPage } from "./Layout/NotFoundPage";
+
+export { Header } from "./Header/Header";
+export { Footer } from "./Footer/Footer";
+
+export { TicketCard } from "./Ticket/TicketCard";
+export { TicketDetails } from "./Ticket/TicketDetails";
+
+export { DashboardSkeleton } from "./Skeleton/dashboard-skeleton";
+export { TicketDetailsSkeleton } from "./Skeleton/ticket-details-skeleton";
