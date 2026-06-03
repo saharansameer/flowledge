aba39777680e3f5cef8fdd5ce855caa01d1ee85b
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2026-06-03 22:10:04 +0530

    chore: migrate inngest sdk to v4 - v3.40.1 -> v4.5.0

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { CLIENT_URL, INNGEST_SIGNING_KEY } from "@/env";
+import { CLIENT_URL } from "@/env";
 
 import { SIZE_LIMIT } from "@/utils/constants";
 import { undefinedRoutesHandler } from "@/middlewares/error.middleware";
@@ -38,7 +38,6 @@ app.use(
   serve({
     client: inngest,
     functions: inngestFunctions,
-    signingKey: INNGEST_SIGNING_KEY,
   })
 );
 
--- server/inngest/client.ts
+++ server/inngest/client.ts
@@ -1,7 +1,8 @@
 import { Inngest } from "inngest";
-import { INNGEST_EVENT_KEY } from "@/env";
+import { INNGEST_EVENT_KEY, INNGEST_SIGNING_KEY } from "@/env";
 
 export const inngest = new Inngest({
   id: "flowledge",
   eventKey: INNGEST_EVENT_KEY,
+  signingKey: INNGEST_SIGNING_KEY,
 });
